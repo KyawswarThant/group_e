8070b1ab7e6d6f728537b4dc7b1fddd31ae660fb
Sensor <sensorgaming19@gmail.com>
2022-01-18 12:51:53 +0630

    add form validations and navigation for pages

--- cafe_ordering_system/app/models/chef.rb
+++ cafe_ordering_system/app/models/chef.rb
@@ -1,3 +1,10 @@
 class Chef < ApplicationRecord
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true 
+  validates :phone, presence: true
+  validates :address, presence: true
+  validates :birthday, presence: true
 end
